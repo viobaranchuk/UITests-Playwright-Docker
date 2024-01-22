e13104b694e1d435b5d5bd4097f1df39473c166e
Violetta Korostashovets <violetta.korostashovets@gmail.com>
2024-01-22 22:34:08 +0100

    snapshots

--- playwright.config.js
+++ playwright.config.js
@@ -23,7 +23,7 @@ module.exports = defineConfig({
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
     // ['list', { printSteps: true }],
-    ['json', {  outputFolder: 'playwright-report', outputFile: 'test-results.json' }],
+    ['json', {  outputFolder: 'playwright-report' }],
     ['html', {  outputFolder: 'playwright-report' }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
